aed43ddba2ac1c5cc1fdd6476847664714e10918
dokizera <waldenir076@gmail.com>
2021-03-15 01:33:00 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Doki:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+      CRIADO POR *DOKI*
+   BOT CAIR ME CHAMEM 👇
+    WA.me/5521982660837
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
